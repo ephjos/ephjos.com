a454729971e166fbc7024867fa2bf6f17f966567
Joseph Hines <jthines3@gmail.com>
2025-11-27 01:12:14 -0500

    add wrapper scripts for arch install scripts

---
+++ public/i.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -L0s "https://raw.githubusercontent.com/ephjos/arch/refs/heads/main/install.sh" | sh
---
+++ public/p.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -L0s "https://raw.githubusercontent.com/ephjos/arch/refs/heads/main/postinstall.sh" | sh
