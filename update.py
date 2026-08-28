13e37c54ae5965e9b44e817845be35bc17fa7dff
Joseph Hines <jthines3@gmail.com>
2026-08-28 05:30:45 -0400

    Update update.py, fix typo

--- update.py
+++ update.py
@@ -1,5 +1,5 @@
 #
-# Update stubs across all .html files
+# Update inline stubs across all .html files
 #
 
 import fileinput
@@ -11,10 +11,13 @@ class Stub:
 
     def __init__(self, name):
         self.name = name
-        self.content = open(f"./stubs/{self.name}.html", "r").read()
         self.start_marker = f"<!-- BEG: {self.name} -->"
         self.end_marker = f"<!-- FIN: {self.name} -->"
-        self.old_marker = "{{"+self.name.upper()+"}}"
+
+        # simple minifier
+        file = open(f"./stubs/{self.name}.html", "r").read()
+        lines = [line.strip() for line in file.split("\n")]
+        self.content = ''.join(lines)
 
 stubs = [Stub("head"), Stub("nav"), Stub("end")]
 
@@ -31,13 +34,7 @@ def update_file(filename):
                     out_lines.append(stub.content)
                     while stub.end_marker not in line:
                         line = next(in_file)
-                    out_lines.append(stub.end_marker+"\n")
-                    break
-                elif stub.old_marker in line:
-                    in_stub = True
-                    out_lines.append(stub.start_marker+"\n")
-                    out_lines.append(stub.content)
-                    out_lines.append(stub.end_marker+"\n")
+                    out_lines.append("\n"+stub.end_marker+"\n")
                     break
             if not in_stub:
                 out_lines.append(line)
@@ -45,7 +42,6 @@ def update_file(filename):
     with open(filename, "w") as out_file:
         for line in out_lines:
             out_file.write(line)
-
 
 def main():
     for root, dirs, files in os.walk("."):
